cada0a2ff1493317dd225a03e1ae3b6f7148bd62
kostyapoymenov <kostya6699@mail.ru>
2019-11-08 16:17:35 +0300

    Тестовое практически готово. ОСталось подогнать время анимаций

--- build/js/main.js
+++ build/js/main.js
@@ -6,20 +6,28 @@ $(document).ready(function(){
   });
 });
 
+// открытие меню
 $('.burger').on('click', function(){
   $('.menu').addClass('menu--visible');
+  $('.menu__btn-back').addClass('menu__btn-back--visible');
   setTimeout(function(){
     $('.menu__content').addClass('menu__content--visible');
-  }, 350);
+  }, 470);
 })
+
+// закрытие меню
 $('.menu__btn-back').on('click', function(e){
   e.preventDefault();
   setTimeout(function(){
     $('.menu').removeClass('menu--visible');
-  }, 300);
+  }, 350);
   $('.menu__content').removeClass('menu__content--visible');
+  setTimeout(function(){
+    $('.menu__btn-back').removeClass('menu__btn-back--visible'); 
+  }, 400);
 })
+
+// открытие aside
 $('.acord__block--one').on('click', function(){
-  // $('.acord__block--two').slideToggle(500);
   $('.acord').toggleClass('acord--active');
 });
